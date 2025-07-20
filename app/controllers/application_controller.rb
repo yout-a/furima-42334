c4c3881b86de6fbb836ceee7b6ffc368c9fbaef0
yout-a <rainyday1388@gmail.com>
2025-07-20 23:59:16 +0900

    itemsコントローラー作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def index
+    @items = Item.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/show'
+  get 'items/new'
+  get 'items/create'
+  get 'items/edit'
+  get 'items/update'
+  get 'items/destroy'
   root "items#index"
   resources :items, only: %i[index show new create edit update destroy]
 end
