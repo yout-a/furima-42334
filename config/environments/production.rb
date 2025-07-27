f9efe68da3b6b7016fcf12f950f95640fa29680a
yout-a <rainyday1388@gmail.com>
2025-07-27 19:36:05 +0900

    production.rbにコード追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "furima-42334.onrender.com"
 end
