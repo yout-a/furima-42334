aac91b65290ad4c04c887884fdd1eb43c9369aa6
yout-a <rainyday1388@gmail.com>
2025-08-01 17:57:36 +0900

    商品画像から詳細ページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,17 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
-
-
-
   def index
+    @items = Item.includes(:user).order(created_at: :desc)
   end
 
-  def new
-    @item = Item.new
-  end
+    def show
+      @item = Item.find(params[:id])
+    end
+
+    def new
+      @item = Item.new
+    end
 
   def create
     @item = Item.new(item_params)
