ead6b9f5df7062fe0657d4ad22256d6fd8ca25eb
yout-a <rainyday1388@gmail.com>
2025-08-11 04:09:19 +0900

    databaseの修正

--- db/migrate/20250726111758_devise_create_users.rb
+++ db/migrate/20250726111758_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", limit: 191
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
+      t.string   :reset_password_token, limit: 191
       t.datetime :reset_password_sent_at
 
       ## Rememberable
@@ -36,8 +36,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true, length: 191
+    add_index :users, :reset_password_token, unique: true, length: 191
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
