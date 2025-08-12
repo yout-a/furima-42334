e2cf0915371981b6ec46a630a005079da2ce38d7
yout-a <rainyday1388@gmail.com>
2025-08-12 22:33:01 +0900

    unicorn.rbの修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,34 +1,23 @@
-app_path = File.expand_path('../../../../', __FILE__)
-
-current = "#{app_path}/current"
-shared  = "#{app_path}/shared"
+app_path = "/var/www/furima-42334"
 
 worker_processes 2
-working_directory current
 
-listen      "#{shared}/tmp/sockets/unicorn.sock"
-pid         "#{shared}/tmp/pids/unicorn.pid"
-stderr_path "#{shared}/log/unicorn.stderr.log"
-stdout_path "#{shared}/log/unicorn.stdout.log"
+working_directory "#{app_path}/current"
+
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+
+pid         "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 timeout 60
 preload_app true
 check_client_connection false
 
-before_fork do |server, worker|
+before_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
 end
 
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
-
-
