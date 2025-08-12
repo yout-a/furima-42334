7a1ee5754370c463b6ad45f6bd6757b3c2e26ec4
yout-a <rainyday1388@gmail.com>
2025-08-12 21:32:49 +0900

    最終調整

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,18 +10,14 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '3.2.3'
 
-# どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/my-key-pair.pem'] 
 
-# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
